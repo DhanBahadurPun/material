ebecefbf24b6b847e7d8fb7a242f7b1ba106f845
DhanBahadurPun <dhanbdrpun295@gmail.com>
2018-04-27 09:07:05 +0545

    changed sessionStorage.getItem

--- src/components/login.js
+++ src/components/login.js
@@ -1,56 +1,75 @@
 import React, { Component } from "react";
 import { MuiThemeProvider } from "material-ui/styles";
 
-class Login extends Component {
+class Login extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       email: "",
       apikey: ""
     };
   }
+
+  render() {
+    return (
+      <div>
+        <Card>
+          <CardHeader title="Login Page" avatar={boy} />
+          <TextField
+            type="email"
+            hintText="Enter email"
+            value={this.state.email}
+            onChange={e => this.onEmailChange(e)}
+          />
+          <br />
+          <TextField
+            type="password"
+            hintText="Enter API Key"
+            value={this.state.apikey}
+            onChange={e => this.onApiKeyChange(e)}
+          />
+          <br />
+          <CardActions>
+            <RaisedButton label="Submit" onClick={() => this.onSubmit()} />
+          </CardActions>
+        </Card>
+      </div>
+    );
+  }
+  // 1a184d14d77f4be4a758aca8f5e72c3b
   onEmailChange(e) {
     this.setState({ email: e.target.value });
   }
+
   onApiKeyChange(e) {
     this.setState({ apikey: e.target.value });
   }
   onSubmit() {
-    fetch("https://api.rebandly.com/vl/account", {
+    fetch("https://api.rebrandly.com/v1/account", {
       headers: {
         apikey: this.state.apikey
       }
-    })
-    .then(response=>{
-      if(response.ok){
-        console.log(response);
-      }else{
-        alert(response.statusText);
+    }).then(response => {
+      if (response.ok) {
+        // console.log(response);
+        response.json().then(data => {
+          if (data.email === this.state.email) {
+            //console.log(data);
+            console.log("Right User!");
+            sessionStorage.getItem("apikey", this.state.apikey);
+            return <Routes />;
+          } else {
+            console.log(data);
+            console.log("Wrong User!");
+          }
+        });
+      } else {
+        alert(response.json());
       }
-    })
-
-    console.log(this.state);
-  }
-  render() {
-    return (
-      <div>
-        <input
-          type="txt"
-          placeholder="Email"
-          value={this.state.email}
-          onChange={e => this.onEmailChange(e)}
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="api key"
-          value={this.state.apikey}
-          onChange={e => this.onApiKeyChange(e)}
-        />
-        <br />
-        <button onClick={() => this.onSubmit()}>Submit</button>
-      </div>
-    );
+    });
+    // .catch((error) => console.log(error));
+    console.log({ email: this.state.email });
   }
 }
 
